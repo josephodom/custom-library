92d80ff1a63a268d15c00259a306524bce2b6707
josephodom <ejosephodom@gmail.com>
2019-01-19 09:52:04 -0500

    Spacing; headers; fonts; some fields work

--- index.php
+++ index.php
@@ -41,81 +41,47 @@ require 'functions.php';
 
 <main id="main">
 	<div class="container">
-		<p>
-			Body.
-		</p>
+		<?php
+		include 'section-flex-table.php';
+		?>
 		
-		<div class="flex-table">
-			<!-- ROW 1 -->
-			<?=cells(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)?>
-			
-			<!-- ROW 2 -->
-			<?=cells(2, 2, 2, 2, 2, 2)?>
-			
-			<!-- ROW 3 -->
-			<?=cells(3, 3, 3, 3)?>
-			
-			<!-- ROW 4 -->
-			<?=cells(4, 4, 4)?>
-			
-			<!-- ROW 5 -->
-			<?=cells(6, 6)?>
-			
-			<!-- ROW 6 -->
-			<?=cells(12)?>
-			
-			<!-- ROW 7 -->
-			<?=cells(7, 5)?>
-			
-			<!-- ROW 8 -->
-			<?=cells(3, 1)?>
-			
-			<div class="cell-4 offset-4 display-none display-block-large">
-				<div class="box-simple">
-					4/12 (offset 3/12)
+		
+		<h1 class="heading-small">
+			Forms
+		</h1>
+		
+		<div class="box padding-large">
+			<form>
+				<div class="field-container">
+					<h2 class="field-title">
+						Your Name
+					</h2>
+					
+					<input type="text" name="your-name" class="field" placeholder="Your Name">
 				</div>
-			</div>
-			
-			<!-- ROW 9 -->
-			<div class="cell-12 cell-large-6">
-				<div class="box-simple">
-					Full on mobile, half on large
+				
+				<div class="field-container">
+					<h2 class="field-title">
+						Your Email
+					</h2>
+					
+					<input type="email" name="your-email" class="field" placeholder="Your Email">
 				</div>
-			</div>
-			
-			<div class="cell-12 cell-large-6">
-				<div class="box-simple">
-					Full on mobile, half on large
+				
+				<div class="field-container">
+					<h2 class="field-title">
+						Choose One
+					</h2>
+					
+					<select name="option" class="field">
+						<option>---</option>
+						<option value="1">One</option>
+						<option value="2">Two</option>
+						<option value="3">Three</option>
+					</select>
 				</div>
-			</div>
-			
-			
-			
-			<!-- ROW 10 -->
-			<div class="cell-6 cell-large-3">
-				<div class="box-simple">
-					Half on mobile, Quarter on large
-				</div>
-			</div>
-			
-			<div class="cell-6 cell-large-3">
-				<div class="box-simple">
-					Half on mobile, Quarter on large
-				</div>
-			</div>
-			
-			<div class="cell-6">
-				<div class="box-simple">
-					Half on all
-				</div>
-			</div>
-			
-			<div class="cell-6 cell-large-12">
-				<div class="box-simple">
-					Half on mobile, full on large
-				</div>
-			</div>
-		</div><!-- .flex-table -->
+			</form>
+		</div>
 	</div>
 </main>
 
---
+++ section-flex-table.php
@@ -0,0 +1,89 @@
+<?php
+
+// Kick people out if they come here
+// index.php defines this constant before including me
+if(!defined('IN_SITE')){
+	header('Location: index.php');
+	die();
+}
+
+?>
+<h1>
+	Flex Table
+</h1>
+
+<p>
+	The <code>.flex-table</code> class is useful for creating responsive layouts, both simple &amp; elaborate!
+</p>
+
+<div class="flex-table">
+	<!-- ROW 1 -->
+	<?=cells(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)?>
+	
+	<!-- ROW 2 -->
+	<?=cells(2, 2, 2, 2, 2, 2)?>
+	
+	<!-- ROW 3 -->
+	<?=cells(3, 3, 3, 3)?>
+	
+	<!-- ROW 4 -->
+	<?=cells(4, 4, 4)?>
+	
+	<!-- ROW 5 -->
+	<?=cells(6, 6)?>
+	
+	<!-- ROW 6 -->
+	<?=cells(12)?>
+	
+	<!-- ROW 7 -->
+	<?=cells(7, 5)?>
+	
+	<!-- ROW 8 -->
+	<?=cells(3, 1)?>
+	
+	<div class="cell-4 offset-4 display-none display-block-large">
+		<div class="box-simple">
+			4/12 (offset 3/12)
+		</div>
+	</div>
+	
+	<!-- ROW 9 -->
+	<div class="cell-12 cell-large-6">
+		<div class="box-simple">
+			Full on mobile, half on large
+		</div>
+	</div>
+	
+	<div class="cell-12 cell-large-6">
+		<div class="box-simple">
+			Full on mobile, half on large
+		</div>
+	</div>
+	
+	
+	
+	<!-- ROW 10 -->
+	<div class="cell-6 cell-large-3">
+		<div class="box-simple">
+			Half on mobile, Quarter on large
+		</div>
+	</div>
+	
+	<div class="cell-6 cell-large-3">
+		<div class="box-simple">
+			Half on mobile, Quarter on large
+		</div>
+	</div>
+	
+	<div class="cell-6">
+		<div class="box-simple">
+			Half on all
+		</div>
+	</div>
+	
+	<div class="cell-6 cell-large-12">
+		<div class="box-simple">
+			Half on mobile, full on large
+		</div>
+	</div>
+</div><!-- .flex-table -->
